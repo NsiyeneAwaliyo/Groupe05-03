bc4b0cd17c1bd25563eaa3ca90e67a966357f1cd
NsiyeneAwaliyo <nsiyenet@gmail.com>
2025-07-15 14:25:34 +0100

    Delete EXO 4.py

--- EXO 4.py
+++
@@ -1,8 +0,0 @@
-# Demander la note sur 20
-note_sur_20 = float(input("Entrez la note sur 20 : "))
-
-# Calculer la note sur 100
-note_sur_100 = note_sur_20 * 5
-
-# Afficher le résultat
-print(f"La note sur 100 est : {note_sur_100}")
